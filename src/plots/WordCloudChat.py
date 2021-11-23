d033c419d6a66410a131c6bd19f2b8126057f8f8
Danmontesi <danmontesi@gmail.com>
2021-11-23 22:03:36 +0100

    finish wordcloud

--- src/plots/WordCloudChat.py
+++ src/plots/WordCloudChat.py
@@ -10,8 +10,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from nltk.corpus import stopwords
 
-nltk.download('stopwords')
-
 from src.preprocessing.CleanDataset import DATASET_PATH
 
 
@@ -38,7 +36,16 @@ class WordCloudChat(ABC):
 
         self.df = self.df[(self.df.date >= start_date) & (self.df.date <= end_date)]
 
-    def clean_text(self, text: str):
+    def get_wordcloud(self, text: str):
+        wc = WordCloud(width=800, height=800,
+                  background_color='white',
+                  min_font_size=10).generate(text)
+        return wc
+
+    def clean_text(self, sender):
+
+        text = ' '.join(self.df[self.df[self.sender_column] == sender][self.text_column].astype('str').tolist())
+
         # clean text removing words having length <= l (l=2 would be enough)
         def remove_short_words(list_words, l=2):
             list_words = [x for x in list_words if len(x) > l]
@@ -49,39 +56,57 @@ class WordCloudChat(ABC):
         list_words = text.split(' ')
         list_words = remove_short_words(list_words)
 
+        to_remove_words = list(set(stopwords.words('italian'))) + ['comunque', 'ora']
         # remove stopwords in italian
-        list_words = [word for word in list_words if word not in list(set(stopwords.words('italian')))]
+        list_words = [word for word in list_words if word not in to_remove_words and ('hah' not in word)]
 
         return ' '.join(list_words)
 
-    def plot(self):
+    def plot_2_wordcloud(self):
         # extract text for the 2 senders
-
         list_senders = self.df[self.sender_column].unique().tolist()
 
-        text_sender_1 = ' '.join(self.df[self.df[self.sender_column] == list_senders[0]][self.text_column].astype('str').tolist())
+        text_sender_1 = self.clean_text(list_senders[0])
+        text_sender_2 = self.clean_text(list_senders[1])
 
-        text_sender_2 = ' '.join(self.df[self.df[self.sender_column] == list_senders[1]][self.text_column].astype('str').tolist())
+        # get wordclouds
+        wc_1 = self.get_wordcloud(text_sender_1)
+        wc_2 = self.get_wordcloud(text_sender_2)
 
-        text_sender_1 = self.clean_text(text_sender_1)
-        text_sender_2 = self.clean_text(text_sender_2)
-        # visualize wordcloud
-        wordcloud = WordCloud(width=800, height=800,
-                              background_color='white',
-                              stopwords=stopwords,
-                              min_font_size=10).generate(text_sender_1 + text_sender_2)
+        list_wc = [wc_1, wc_2]
 
+        plt.figure(figsize=(10, 10))
+
+        for i in range(len(list_wc)):
+            plt.subplot(2, 1, i+1).set_title("Sender: " + list_senders[i])
+            plt.plot()
+
+            plt.imshow(list_wc[i])
+            plt.axis('off')
+
+        plt.suptitle('WordCloud Chat from {} to {}'.format(self.start_date, self.end_date))
+        plt.show()
+
+    def plot_single_wordcloud(self):
+        # extract text for the 2 senders
+        list_senders = self.df[self.sender_column].unique().tolist()
+
+        text_sender_1 = self.clean_text(list_senders[0])
+        text_sender_2 = self.clean_text(list_senders[1])
+
+        wordcloud = self.get_wordcloud(text_sender_1 + text_sender_2)
         # plot the WordCloud image
         plt.figure(figsize=(8, 8), facecolor=None)
         plt.imshow(wordcloud)
         plt.axis("off")
         plt.tight_layout(pad=0)
-
+        plt.title('WordCloud Chat from {} to {}'.format(self.start_date, self.end_date))
         plt.show()
 
 
 if __name__ == "__main__":
     df = pd.read_csv(DATASET_PATH + 'final_dataset.csv')
+    print(stopwords.words('italian'))
+    wc = WordCloudChat(df, '2017-01-01', '2019-01-01', 'message', 'sender')
 
-    wc = WordCloudChat(df, '2015-01-01', '2016-01-01', 'message', 'sender')
-    wc.plot()
+    wc.plot_2_wordcloud()
