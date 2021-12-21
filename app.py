793ec85aa23d935378ac607c163e80354a18924f
Nicola Bini <63654846+Nicola-Bini@users.noreply.github.com>
2021-12-21 15:52:47 -0500

    Choose graph frequency Bug: change chat not working Change EmojisPlot class to Function

--- app.py
+++ app.py
@@ -10,114 +10,93 @@ from dash.dependencies import Input, Output, State
 import pandas as pd
 import os
 
-from src.plots.EmojiPlots import CountEmojis
+from src.plots.EmojiPlots import EmojisPlot
 from src.main import main
 
+app = dash.Dash(__name__)
 
-class App():
+# Load chats
+raw_chats = os.listdir('./src/dataset')
 
-    def main(self):
-    app = dash.Dash(__name__)
+# Remove working files
+working_csv = ['final_dataset.csv', 'dataset.csv']
 
-    # Load chats
-    raw_chats = os.listdir('./src/dataset')
+for file in working_csv:
+    if file in raw_chats:
+        raw_chats.remove(file)
 
-    # Remove working files
-    working_csv = ['final_dataset.csv', 'dataset.csv']
+chats = list(map(str.strip, raw_chats))
 
-    for file in working_csv:
-        if file in raw_chats:
-            raw_chats.remove(file)
+app.layout = html.Div(id='header',children=[
+    html.Div(id='nav-bar', children=[
+        html.Img(id='logo',
+                 src=app.get_asset_url('images/logo.png'),
+                 style={'max-width':'15em',
+                        'margin-left':'5px',
+                        'margin-top': '5px'})
+    ], style={'background-color':'white'}),
 
-    chats = list(map(str.strip, raw_chats))
+    html.Div(id='body',
+             children=[
+                 dcc.Dropdown(id='chat-dropdown',
+                              options=[{'label':x[1:], 'value': x} for i, x in enumerate(chats)],
+                              value = chats[0],
+                              style = {'width':'20em',
+                                       'margin-right':'10px'}),
+                 html.Button('Load Data', id='load-data', n_clicks=0)
+             ], style={'display':'flex',
+                       'justify-content': 'start',
+                       'margin':'15px 10px'}),
+    html.Div(id='dashboard',
 
-    app.layout = html.Div(id='body',children=[
-        html.Div(id='header', children=[
-            html.Img(id='logo',
-                     src=app.get_asset_url('images/logo.png'),
-                     style={'max-width':'15em',
-                            'margin-left':'5px',
-                            'margin-top': '5px'})
-        ], style={'background-color':'white'}),
+             children=[
+                html.Div(id='dashboard-left',
+                         children=[
+                             dcc.Dropdown(id='frequency-dropdown',
+                                          options=[{'label': x, 'value': x} for x in ['day','month', 'all']],
+                                          value=chats[0],
+                                          style={'width': '20em',
+                                                 'margin-right': '10px'}),
+                             html.Button('Load Data', id='choose-frequency', n_clicks=0)
+                         ],
+                         style={'width':'50%',
+                                'background-color':'blue'}),
+                html.Div(id='dashboard-right',
+                         children=[],
+                         style={'width':'50%'})
+            ], style={'display':'flex'}
+    )])
 
-        html.Div(id='nav-bar',
-                 children=[
-                     dcc.Dropdown(id='chat-dropdown',
-                                  options=[{'label':x[1:], 'value': x} for i, x in enumerate(chats)],
-                                  value = chats[0],
-                                  style = {'width':'20em',
-                                           'margin-right':'10px'}),
-                     html.Button('Load Data', id='load-data', n_clicks=0)
-                 ], style={'display':'flex',
-                           'justify-content': 'start',
-                           'margin':'15px 10px'}),
-        html.Div(id='dashboard',
+@app.callback(
+    Input('load-data', 'n_clicks'),
+    State('chat-dropdown', 'value'))
+def load_chat(n_clicks, value):
+    ctx = dash.callback_context
 
-                 children=[
-                    html.Div(id='dashboard-left',
-                             children=[
-                                 dcc.Dropdown(id='frequency-dropdown',
-                                              options=[{'label': x[1:], 'value': x} for x in ['day','month', 'all']],
-                                              value=chats[0],
-                                              style={'width': '20em',
-                                                     'margin-right': '10px'}),
-                                 html.Button('Load Data', id='choose-frequency', n_clicks=0)
-                             ],
-                             style={'width':'50%',
-                                    'background-color':'blue'}),
-                    html.Div(id='dashboard-right',
-                             children=[],
-                             style={'width':'50%'})
-                ], style={'display':'flex'}
-        )])
+    if not ctx.triggered:
+        return
 
-
-
-
-
-
-
-    @app.callback(
-        Output('dashboard-left', 'children'),
-        Input('load-data', 'n_clicks'),
-        State('chat-dropdown', 'value'))
-    def load_data(n_clicks, value):
-        ctx = dash.callback_context
+    main(value, debug=True)
 
 
-        graphs = []
+@app.callback(
+    Output('dashboard-right', 'children'),
+    Input('choose-frequency', 'n_clicks'),
+    State('frequency-dropdown', 'value'))
+def change_frequency(n_clicks, value):
 
-        if not ctx.triggered:
-            return
+    df = pd.read_csv('./src/dataset/final_dataset.csv')
 
-        main(value, debug=True)
-        df = pd.read_csv('./src/dataset/final_dataset.csv')
+    emojis_fig = EmojisPlot(df, value)
 
-
-
-    @app.callback(
-        Output('dashboard-left', 'children'),
-        Input('choose-frequency', 'n_clicks'),
-        State('frequency-dropdown', 'value'))
-    def load_data(n_clicks, value):
-        emojis_fig = go.Figure()
-
-        graphs = []
-
-        if value == 'month':
-            for sender, sender_emojis in self.df.groupby('sender'):
-                emojis_fig.add_scatter(x=sender_emojis.year_month, y=sender_emojis.emojis_count, name = sender)
-
-        elif value == 'day':
-            for sender, sender_emojis in self.df.groupby('sender'):
-                emojis_fig.add_scatter(x=sender_emojis.date, y=sender_emojis.emojis_count, name = sender)
-
-        elif value == 'all':
-            for sender, sender_emojis in emojis_fig.df.groupby('sender'):
-                emojis_fig.add_bar(x=sender_emojis.sender, y=sender_emojis.emojis_count, name = sender)
+    graphs = []
+    graphs.append(dcc.Graph(figure=emojis_fig))
+    #graphs.append(dcc.Graph(figure=danisss_fig))
 
 
-        graphs.append(dcc.Graph(figure=emojis_fig))
+    return graphs
 
-        return graphs
 
+
+
+app.run_server(debug=False)
--- src/plots/EmojiPlots.py
+++ src/plots/EmojiPlots.py
@@ -3,54 +3,65 @@ import plotly.graph_objs as go
 from emoji import UNICODE_EMOJI
 
 pd.set_option('display.max_columns', 500)
-class CountEmojis():
-    def __init__(self, df, emojis='love'):
+def EmojisPlot(df_data, value, emojis='love'):
 
+    # emojis_lst = []
+    # [emojis_lst.append(x[0]) for x in list(UNICODE_EMOJI['en'])]
+    if emojis == 'love':
+        emojis_lst = ['❤', '🥰', '😍', '😘', '💕', '💙', '💜', '❣', '💚', '💞', '💖', '💛', \
+                       '🖤', '💗', '💌', '💓', '😻', '🧡', '💘', '💝', '😽', '‍💟', '‍❤', '️💏', \
+                       '🏩', '💒', '🤎', '🤍', '‍💒']
 
-        # emojis_lst = []
-        # [emojis_lst.append(x[0]) for x in list(UNICODE_EMOJI['en'])]
-        if emojis == 'love':
-            emojis_lst = ['❤', '🥰', '😍', '😘', '💕', '💙', '💜', '❣', '💚', '💞', '💖', '💛', \
-                           '🖤', '💗', '💌', '💓', '😻', '🧡', '💘', '💝', '😽', '‍💟', '‍❤', '️💏', \
-                           '🏩', '💒', '🤎', '🤍', '‍💒']
+    df = df_data
+    df['emojis_count'] = 0
+    multi_index = [df['date'], df['sender']]
 
-        self.df = df
-        self.df['emojis_count'] = 0
-        multi_index = [df['date'], df['sender']]
+    for emoji in emojis_lst:
+        # Emoji count per message
+        df['emojis_count'] = df['emojis_count'] + df.message.str.count(emoji)
 
-        for emoji in emojis_lst:
-            # Emoji count per message
-            self.df['emojis_count'] = self.df['emojis_count'] + self.df.message.str.count(emoji)
+    # Emoji count per month
+    monthly_df = df.groupby(by=["year_month", "sender"])['emojis_count'].sum().reset_index()
+    df = pd.merge(df_data,
+                  monthly_df,
+                  how='left',
+                  left_on=['year_month', 'sender'],
+                  right_on=['year_month','sender'],
+                  suffixes=[None, '_monthly'])
 
-        # Emoji count per month
-        monthly_df = self.df.groupby(by=["year_month", "sender"])['emojis_count'].sum().reset_index()
-        self.df = pd.merge(df,
-                      monthly_df,
-                      how='left',
-                      left_on=['year_month', 'sender'],
-                      right_on=['year_month','sender'],
-                      suffixes=[None, '_monthly'])
+    # Emoji count per day
+    daily_df= df.groupby(by=["date", "sender"]).count()['emojis_count'].reset_index()
+    df = pd.merge(df_data,
+                  daily_df,
+                  how='left',
+                  left_on=['date', 'sender'],
+                  right_on=['date','sender'],
+                  suffixes=[None, '_daily'])
 
-        # Emoji count per day
-        daily_df= self.df.groupby(by=["date", "sender"]).count()['emojis_count'].reset_index()
-        self.df = pd.merge(df,
-                      daily_df,
-                      how='left',
-                      left_on=['date', 'sender'],
-                      right_on=['date','sender'],
-                      suffixes=[None, '_daily'])
+    # Emoji count for the whole period
+    all_emojis = df.groupby(by=["sender"]).count()['emojis_count'].reset_index()
+    df = pd.merge(df_data,
+                  all_emojis,
+                  how='left',
+                  left_on=['sender'],
+                  right_on=['sender'],
+                  suffixes=[None, '_all'])
 
-        # Emoji count for the whole period
-        all_emojis = self.df.groupby(by=["sender"]).count()['emojis_count'].reset_index()
-        self.df = pd.merge(df,
-                      all_emojis,
-                      how='left',
-                      left_on=['sender'],
-                      right_on=['sender'],
-                      suffixes=[None, '_all'])
+    emojis_fig = go.Figure()
 
+    if value == 'month':
+        for sender, sender_emojis in df.groupby('sender'):
+            emojis_fig.add_scatter(x=sender_emojis.year_month, y=sender_emojis.emojis_count, name=sender)
 
+    elif value == 'day':
+        for sender, sender_emojis in df.groupby('sender'):
+            emojis_fig.add_scatter(x=sender_emojis.date, y=sender_emojis.emojis_count, name=sender)
 
+    elif value == 'all':
+        for sender, sender_emojis in df.groupby('sender'):
+            emojis_fig.add_bar(x=sender_emojis.sender, y=sender_emojis.emojis_count, name=sender)
+
+    return emojis_fig
 
 if __name__ == '__main__':
     df = pd.read_csv('../dataset/final_dataset.csv')
